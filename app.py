af56f0addfbcc261747c0f59a78ca0d1dd5bea86
Sylwia <sylwia.godlewska@uth.pl>
2026-03-20 19:10:03 +0100

    Dodano interaktywny panel dziecka GlikoKids z obliczeniami WW/WBT

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    # To polecenie szuka pliku rodzic.html w folderze 'templates'
+# Strona Rodzica (Strona główna)
+@app.route('/rodzic')
+def rodzic():
     return render_template('rodzic.html')
+
+# Strona Dziecka
+@app.route('/')
+def dziecko():
+    return render_template('dziecko.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
